d1b06c2d8234f07848ae06c039b722a1010aa3be
GaganDeep Tomar <gagandeep280597@gmail.com>
2019-09-12 12:25:43 +0530

    testing

---
+++ Welcome to PyGlow’s documentation! — PyGlow 0.1.7 documentation_files/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "pyglow",
+    version: "latest",
+    language: "en",
+    programming_language: "words",
+    subprojects: {},
+    canonical_url: "https://pyglow.readthedocs.io/en/latest/",
+    theme: "alabaster",
+    builder: "sphinx",
+    docroot: "/docs/source/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "8ea42002",
+    ad_free: false,
+
+    global_analytics_code: 'UA-17997319-1',
+    user_analytics_code: null
+};
+
